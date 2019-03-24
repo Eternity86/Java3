d1f8742f5fd3a78bf2a8e0d5ae1cdcd3b4ea0410
Aleksandr <41491165+Eternity86@users.noreply.github.com>
2019-03-24 19:05:20 +0300

    добавлена проверка на валидность передающихся в метод индексов и вывод сообщения об ошибке в случае неправильных индексов

--- src/Java3.lessons/lesson1/task1/SwapElems.java
+++ src/Java3.lessons/lesson1/task1/SwapElems.java
@@ -1,4 +1,18 @@
 package Java3.lessons.lesson1.task1;
 
 public class SwapElems {
+	public static void main(String[] args) {
+
+	}
+
+	public static void swap(Object[] arr, int indexOne, int indexTwo) {
+		if (indexOne > 0 && indexOne <= arr.length && indexTwo > 0 && indexTwo <= arr.length) {
+			Object temp = arr[indexOne - 1];
+			arr[indexOne - 1] = arr[indexTwo - 1];
+			arr[indexTwo - 1] = temp;
+		} else {
+			System.out.println("Выход за пределы массива. Индекс элемента массива должен быть от 1 до " + arr.length);
+		}
+
+	}
 }
