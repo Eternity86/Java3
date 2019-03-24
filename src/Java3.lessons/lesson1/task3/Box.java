a1c07f49f0fb9214af78a914260ad097be0164e5
Aleksandr <41491165+Eternity86@users.noreply.github.com>
2019-03-24 20:45:19 +0300

    Класс Коробка, в которой хранятся только фрукты одного типа. Метод, возвращающий вес коробки, сравнивающий две коробки по весу, пересыпающий фрукты из одной коробки в другую.

--- src/Java3.lessons/lesson1/task3/Box.java
+++ src/Java3.lessons/lesson1/task3/Box.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class Box<T extends Fruit> {
 	private ArrayList<T> list;
 
+	@SafeVarargs
 	public Box(T... arr) {
 		list = new ArrayList<>(Arrays.asList(arr));
 	}
@@ -20,9 +21,10 @@ public class Box<T extends Fruit> {
 	}
 
 	public boolean compare(Box another) {
-		return Math.abs(this.getWeight() - another.getWeight()) < 0.00001;
+		return Math.abs(this.getWeight() - another.getWeight()) < 0.00001f;
 	}
 
+	//при пересыпании фруктов из коробки в коробку ограничим другую коробку только фруктами того же типа, чтобы избежать "смешивания" фруктов
 	public void shiftFruits(Box<? super T> another) {
 		another.list.addAll(this.list);
 		this.list.clear();
