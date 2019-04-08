0cd7d413ff0a52fb1b13693efeb713796a673d18
Infinity2010 <arbiter2008@ya.ru>
2019-04-08 14:11:47 +0300

    Запускаем участники-потоки и используем CyclicBarrier для синхронизации

--- src/Java3.lessons/lesson5/task1/MainClass.java
+++ src/Java3.lessons/lesson5/task1/MainClass.java
@@ -1,19 +1,30 @@
 package lesson5.task1;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainClass {
-    private static final int CARS_COUNT = 4;
+    static final int CARS_COUNT = 8;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier stopLine = new CyclicBarrier(CARS_COUNT + 1);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), stopLine);
         }
         for (Car car : cars) {
             new Thread(car).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            stopLine.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            stopLine.await();
+            stopLine.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 
