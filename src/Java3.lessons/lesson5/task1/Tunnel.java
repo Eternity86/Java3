0d2c66aab3b84f1999379efa4ad7d5d6d6f62eb3
Infinity2010 <arbiter2008@ya.ru>
2019-04-08 14:01:57 +0300

    в класс Туннель вводим синхронизатор Семафор, который за раз будет впускать в туннель половину участников

--- src/Java3.lessons/lesson5/task1/Tunnel.java
+++ src/Java3.lessons/lesson5/task1/Tunnel.java
@@ -1,6 +1,10 @@
 package lesson5.task1;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    // в туннель одновременно могут заехать только половина участников
+    private Semaphore semaphore = new Semaphore(MainClass.CARS_COUNT / 2);
 
     Tunnel() {
         this.length = 80;
@@ -11,12 +15,14 @@ public class Tunnel extends Stage {
     public void go(Car c) {
         try {
             System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+            semaphore.acquire();
             System.out.println(c.getName() + " начал этап: " + description);
             Thread.sleep(length / c.getSpeed() * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             System.out.println(c.getName() + " закончил этап: " + description);
+            semaphore.release();
         }
     }
 
