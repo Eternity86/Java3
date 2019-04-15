10f7c1711da13f2042759648c7cd2837f3bcd122
Infinity86 <arbiter2008@ya.ru>
2019-04-15 13:39:19 +0300

    Интерефейсы-аннотации применяются к методам и информация о них доступна в рантайме

--- src/Java3.lessons/lesson7/Test.java
+++ src/Java3.lessons/lesson7/Test.java
@@ -1,4 +1,14 @@
 package lesson7;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Test {
+    int DEFAULT_PRIORITY = 5;
+    // создаём приоритет для аннотации, по умолчанию 5
+    int priority() default DEFAULT_PRIORITY;
 }
