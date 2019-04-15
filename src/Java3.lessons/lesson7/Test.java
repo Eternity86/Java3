6c5b8a8a7f7e6cb938e5262a80a968ed9f8df199
Infinity2010 <arbiter2008@ya.ru>
2019-04-15 19:37:40 +0300

    мелкие правки по стилю кода

--- src/Java3.lessons/lesson7/Test.java
+++ src/Java3.lessons/lesson7/Test.java
@@ -8,7 +8,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Test {
+    // создаём приоритет для аннотации
     int DEFAULT_PRIORITY = 5;
-    // создаём приоритет для аннотации, по умолчанию 5
+
     int priority() default DEFAULT_PRIORITY;
 }
